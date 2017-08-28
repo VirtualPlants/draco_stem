da67548036ee237b8e48bba3c442fa68512edd47
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2017-08-28 10:51:29 +0100

    added more layers in example image + updated oalab example

--- oalab/draco-stem/model/test_draco.py
+++ oalab/draco-stem/model/test_draco.py
@@ -11,8 +11,9 @@ from openalea.oalab.colormap.colormap_def import load_colormaps
 world.clear()
 
 size = 100.
-n_points = 31
-img = sphere_tissue_image(size=size, n_points=n_points)
+n_points = 11
+n_layers = 2
+img = sphere_tissue_image(size=size, n_points=n_points, n_layers=n_layers)
 
 world.add(img,"segmented_image",colormap='glasbey',alphamap='constant',bg_id=1,alpha=0.25)
 
@@ -23,24 +24,41 @@ world.add(draco.triangulation_topomesh,'adjacency_complex')
 world['adjacency_complex']['display_3'] = False
 world['adjacency_complex']['display_0'] = True
 world['adjacency_complex_vertices']['display_colorbar'] = False
+world['adjacency_complex_vertices']['polydata_colormap'] = load_colormaps()['glasbey']
+world['adjacency_complex_vertices']['point_radius'] = 1.5
 
 draco.adjacency_complex_optimization(n_iterations=3)
 
-world['adjacency_complex']['coef_3'] = 0.95
+world['adjacency_complex']['coef_3'] = 0.9
 world['adjacency_complex']['display_3'] = True
 world['adjacency_complex_cells']['display_colorbar'] = False
 world['adjacency_complex_cells']['polydata_colormap'] = load_colormaps()['grey']
 world['adjacency_complex_cells']['intensity_range'] = (-1,0)
 world['adjacency_complex_cells']['preserve_faces'] = True
-world['adjacency_complex_cells']['x_slice'] = (0,80)
+world['adjacency_complex_cells']['x_slice'] = (0,90)
 
 triangular = ['star','remeshed','projected','flat']
 image_dual_topomesh = draco.dual_reconstruction(reconstruction_triangulation = triangular, adjacency_complex_degree=3, maximal_edge_length=5.1)
+
+
+
+from openalea.cellcomplex.property_topomesh.property_topomesh_analysis import compute_topomesh_property, compute_topomesh_vertex_property_from_faces
+
+compute_topomesh_property(image_dual_topomesh,'barycenter',2)
+compute_topomesh_property(image_dual_topomesh,'normal',2,normal_method='orientation')
+
+compute_topomesh_vertex_property_from_faces(image_dual_topomesh,'normal',adjacency_sigma=2,neighborhood=5)
+compute_topomesh_property(image_dual_topomesh,'mean_curvature',2)
+compute_topomesh_vertex_property_from_faces(image_dual_topomesh,'mean_curvature',adjacency_sigma=2,neighborhood=5)
+compute_topomesh_vertex_property_from_faces(image_dual_topomesh,'gaussian_curvature',adjacency_sigma=2,neighborhood=5)
 
 world.add(image_dual_topomesh ,'dual_reconstruction')
 world['dual_reconstruction']['coef_3'] = 0.99
 world['dual_reconstruction_cells']['display_colorbar'] = False
 world['dual_reconstruction_cells']['x_slice'] = (0,40)
-
+world['dual_reconstruction']['display_1'] = True
+world['dual_reconstruction_edges']['display_colorbar'] = False
+world['dual_reconstruction_edges']['polydata_alpha'] = 0.1
+world['dual_reconstruction_edges']['x_slice'] = (0,40)
 
 
--- src/openalea/draco_stem/example_image.py
+++ src/openalea/draco_stem/example_image.py
@@ -27,54 +27,84 @@ from openalea.container import array_dict
 
 from openalea.image.spatial_image import SpatialImage
 
-def sphere_tissue_image(size=100, n_points=12):
+def sphere_tissue_image(size=100, n_points=12, n_layers=1):
 
     center = np.array([size/2,size/2,size/2],float)
-    radius = size/4.
-
-    points = {}
-    for p in range(n_points):
-        theta = np.random.rand()*2.*np.pi
-        phi = np.random.rand()*np.pi - np.pi/2.
-        
-        points[p+3] = center + radius*np.array([np.cos(theta)*np.cos(phi),np.sin(theta)*np.cos(phi),np.sin(phi)])
-    points = array_dict(points)
-
-    point_target_area = 4.*np.pi*np.power(radius,2.)/float(n_points)
-    point_target_distance = np.power(point_target_area/np.pi,0.5)
-
-    sigma_deformation = (size/100.)*(20./n_points)
-    omega_forces = dict(distance=0.1*size/100., repulsion=100.0*np.power(size/100.,2))
-
-    for iterations in xrange(100):
-        point_vectors = np.array([points[p]- points.values() for p in points.keys()])
-        point_distances = np.array([vq(points.values(),np.array([points[p]]))[1] for p in points.keys()])
-        point_vectors = point_vectors/(point_distances[...,np.newaxis]+1e-7)
-
-        point_distance_forces = omega_forces['distance']*((point_distances-point_target_distance)[...,np.newaxis]*point_vectors/point_target_distance).sum(axis=1)
-        
-        point_repulsion_forces = omega_forces['repulsion']*np.power(point_target_distance,2)*(point_vectors/(np.power(point_distances,2)+1e-7)[...,np.newaxis]).sum(axis=1)
-        
-        point_forces = np.zeros((len(points),3))
-        point_forces += point_distance_forces
-        point_forces += point_repulsion_forces
-        
-        point_forces = np.minimum(1.0,sigma_deformation/np.linalg.norm(point_forces,axis=1))[:,np.newaxis] * point_forces
-        
-        new_points = points.values() + point_forces
-        
-        new_points = center+ radius*((new_points-center)/np.linalg.norm((new_points-center),axis=1)[:,np.newaxis])
-        
-        points = array_dict(new_points,points.keys())
-    points[2] = center
 
     coords = np.transpose(np.mgrid[0:size,0:size,0:size],(1,2,3,0)).reshape((np.power(size,3),3)).astype(int)
-    labels = points.keys()[vq(coords,points.values())[0]]
+    coords_distances = np.linalg.norm(coords-center,axis=1)
 
-    ext_coords = coords[vq(coords,np.array([center]))[1]>size/3.]
+    points = {}
+    layer_img = {}
+
+    for layer in xrange(n_layers):
+    
+        radius = (layer+1)*size/float(2*n_layers+1)
+
+        layer_n_points = n_points*np.power(layer+1,2)
+
+        layer_points = {}
+
+        for p in range(layer_n_points):
+            theta = np.random.rand()*2.*np.pi
+            phi = np.random.rand()*np.pi - np.pi/2.
+            
+            layer_points[p+np.power(layer,2)*n_points+3] = center + radius*np.array([np.cos(theta)*np.cos(phi),np.sin(theta)*np.cos(phi),np.sin(phi)])
+            layer_points = array_dict(layer_points)
+
+            point_target_area = 4.*np.pi*np.power(radius,2.)/float(layer_n_points)
+            point_target_distance = np.power(point_target_area/np.pi,0.5)
+
+            sigma_deformation = (size/100.)*(20./layer_n_points)
+            omega_forces = dict(distance=0.1*size/100., repulsion=100.0*np.power(size/100.,2))
+
+            for iterations in xrange(100):
+                point_vectors = np.array([layer_points[p]- layer_points.values() for p in layer_points.keys()])
+                point_distances = np.array([vq(layer_points.values(),np.array([layer_points[p]]))[1] for p in layer_points.keys()])
+                point_vectors = point_vectors/(point_distances[...,np.newaxis]+1e-7)
+
+                point_distance_forces = omega_forces['distance']*((point_distances-point_target_distance)[...,np.newaxis]*point_vectors/point_target_distance).sum(axis=1)
+                
+                point_repulsion_forces = omega_forces['repulsion']*np.power(point_target_distance,2)*(point_vectors/(np.power(point_distances,2)+1e-7)[...,np.newaxis]).sum(axis=1)
+                
+                point_forces = np.zeros((len(layer_points),3))
+                point_forces += point_distance_forces
+                point_forces += point_repulsion_forces
+                
+                point_forces = np.minimum(1.0,sigma_deformation/np.linalg.norm(point_forces,axis=1))[:,np.newaxis] * point_forces
+                
+                new_points = layer_points.values() + point_forces
+                
+                new_points = center+ radius*((new_points-center)/np.linalg.norm((new_points-center),axis=1)[:,np.newaxis])
+                
+                layer_points = array_dict(new_points,layer_points.keys())
+
+            for p in layer_points.keys():
+                points[p] = layer_points[p]
+
+            labels = layer_points.keys()[vq(coords,layer_points.values())[0]]
+
+            layer_img[layer+1] = np.ones((size,size,size),np.uint8)
+            layer_img[layer+1][tuple(np.transpose(coords))] = labels
+
+
+    points[2] = center
+    points = array_dict(points)
+
+    # coords = np.transpose(np.mgrid[0:size,0:size,0:size],(1,2,3,0)).reshape((np.power(size,3),3)).astype(int)
+    # labels = points.keys()[vq(coords,points.values())[0]]
+
 
     img = np.ones((size,size,size),np.uint8)
-    img[tuple(np.transpose(coords))] = labels
+
+    for layer in xrange(n_layers):
+        layer_coords = coords[(coords_distances > (2*layer+1)*size/float(4*(n_layers+1))) & (coords_distances <= (2*layer+3)*size/float(4*(n_layers+1)))]
+        img[tuple(np.transpose(layer_coords))] = layer_img[layer+1][tuple(np.transpose(layer_coords))]
+
+    center_coords = coords[coords_distances <= size/float(4*(n_layers+1))]
+    img[tuple(np.transpose(center_coords))] = 2
+
+    ext_coords = coords[coords_distances > (n_layers+1)*size/float(2*(n_layers+2))]
     img[tuple(np.transpose(ext_coords))] = 1
     img = SpatialImage(img,voxelsize=(60./size,60./size,60./size))
 
